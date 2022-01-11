50a42a0811523b78937afd8131f2022277053f77
LorranPdS <lorransantospereira@yahoo.com.br>
2022-01-11 11:26:04 -0300

    aula 30 - verificando comportamentos, parte 2

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ This is an example project from the classic video store to practice unit testing
 ATALHO PARA EXECUÇÃO DO TESTE: Ctrl + F11 com o cursor em cima do nome do teste
 Oriente-se pelos commits exibidos no Github para seguir a sequência de aulas
 
-Aula 29 - Verificando comportamentos
+Aula 30 - Verificando comportamentos, parte 2
 
-Agora o sistema deve: enviar email para notificar os clientes com pagamentos atrasados
-
-- você não precisa fazer um microgerenciamento de todas as chamadas pelo verify do mockito, apenas o necessário.
+Tivemos um problema na última aula com notificações a quem está com pagamento atrasado, ou seja, locações no prazo também estão recebendo email. Vamos resolver isso.
--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -2,6 +2,7 @@ package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.builders.FilmeBuilder.umFilme;
 import static br.ce.wcaquino.builders.FilmeBuilder.umFilmeSemEstoque;
+import static br.ce.wcaquino.builders.LocacaoBuilder.umLocacao;
 import static br.ce.wcaquino.builders.UsuarioBuilder.umUsuario;
 import static br.ce.wcaquino.matchers.MatchersProprios.caiNumaSegunda;
 import static br.ce.wcaquino.matchers.MatchersProprios.ehHoje;
@@ -9,6 +10,9 @@ import static br.ce.wcaquino.matchers.MatchersProprios.ehHojeComDiferencaDias;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -25,7 +29,6 @@ import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
 
-import br.ce.wcaquino.builders.LocacaoBuilder;
 import br.ce.wcaquino.daos.LocacaoDao;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -139,7 +142,8 @@ public class LocacaoServiceTest {
 //		Usuario usuario2 = umUsuario().comNome("Usuario 2").agora();
 		List<Filme> filmes = Arrays.asList(umFilme().agora());
 		
-		when(spc.possuiNegativacao(usuario)).thenReturn(true);
+//		when(spc.possuiNegativacao(usuario)).thenReturn(true); // ao invés de usar esse, vamos usar o matcher abaixo do any() para deixar mais genérico
+		when(spc.possuiNegativacao(Mockito.any(Usuario.class))).thenReturn(true);
 		
 		// ação
 		try {
@@ -159,21 +163,43 @@ public class LocacaoServiceTest {
 	public void deveEnviarEmailParaLocacoesAtrasadas() {
 		// cenário
 		Usuario usuario = umUsuario().agora();
-//		Usuario usuario2 = umUsuario().comNome("Usuario 2").agora(); //-> essa seria a linha do unhappy path para termos certeza que não está sendo gerado um falso positivo
+		Usuario usuario2 = umUsuario().comNome("Usuario em dia").agora(); //-> essa seria a linha do unhappy path para termos certeza que não está sendo gerado um falso positivo
+		Usuario usuario3 = umUsuario().comNome("Outro atrasado").agora();
 		
 		List<Locacao> locacoes = Arrays.asList(
-				LocacaoBuilder.umLocacao()
-				.comUsuario(usuario)
-				.comDataRetorno(DataUtils.obterDataComDiferencaDias(-2))
-				.agora());
+				umLocacao()
+					.atrasado()
+					.comUsuario(usuario)
+					.agora(),
+				umLocacao()
+					.comUsuario(usuario2)
+					.agora(),
+				umLocacao()
+					.atrasado()
+					.comUsuario(usuario3)
+					.agora(),
+				umLocacao() // aqui seria uma situação em que o usuário 3 recebesse 2 emails de notificação
+					.atrasado()
+					.comUsuario(usuario3)
+					.agora()
+					);
 		
-		Mockito.when(dao.obterLocacoesPendentes()).thenReturn(locacoes);
+		when(dao.obterLocacoesPendentes()).thenReturn(locacoes);
 		
 		// ação
 		service.notificarAtrasos();
 		
 		// verificação
-		Mockito.verify(email).notificarAtraso(usuario); // veja que fizemos uma verificação do mesmo método descrito na ação
-//		Mockito.verify(email).notificarAtraso(usuario2); //-> aqui você coloca o usuário que seria do unhappy path
+		verify(email, times(3)).notificarAtraso(Mockito.any(Usuario.class)); // esse any() não serve apenas para objetos, mas você verificando-o, verá que serve para boolean, double, int, ...
+		verify(email).notificarAtraso(usuario); // veja que fizemos uma verificação do mesmo método descrito na ação
+
+//		verify(email, Mockito.times(2)).notificarAtraso(usuario3); // possibilidade 1: verifica 2 vezes a notificação
+//		verify(email, Mockito.atLeast(2)).notificarAtraso(usuario3); // possibilidade 2: verifica pelo menos 2 notificação
+//		verify(email, Mockito.atMost(5)).notificarAtraso(usuario3); // possibilidade 3: verifica no máximo 5 notificações
+		verify(email, Mockito.atLeastOnce()).notificarAtraso(usuario3); // possibilidade 4: verifica pelo menos 1 (não importa quantos emails forem enviados, se passar 1 já é considerado)
+		
+		verify(email, Mockito.never()).notificarAtraso(usuario2); // com o aviso no Mockito, eu digo que não quero que o usuario2 seja notificado com atraso porque ele não tem atraso
+		verifyNoMoreInteractions(email);
+//		verifyZeroInteractions(spc); // não vai precisar na prática, mas deixei anotado para saber que existe
 	}
 }
